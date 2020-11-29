2c9aefb62ed8fb46aa85ec58f7d7281c750634df
Diogo Gomes <diogo.gomes77@gmail.com>
2020-11-29 21:29:22 +0000

    added scripts cleaning and materialize

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    // Init Mateliialize JS
+    M.AutoInit();
+  });
+  return <div className="App">My app</div>;
+};
+
+export default App;
