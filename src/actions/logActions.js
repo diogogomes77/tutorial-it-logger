006829402c4b7c6c2e2dba7b5e46b88eaebd58ab
Diogo Gomes <diogo.gomes77@gmail.com>
2020-12-06 20:06:40 +0000

    added logReducer

---
+++ src/actions/logActions.js
@@ -0,0 +1,36 @@
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  UPDATE_LOG,
+  SEARCH_LOGS,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+} from './types';
+
+export const getLogs = () => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch('/logs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+// Set loading to true (sending to logReducer)
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
