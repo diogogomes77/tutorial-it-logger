16b163bb98d8e7709b90d6ea0f9c573172a4c411
Diogo Gomes <diogo.gomes77@gmail.com>
2020-12-07 14:25:44 +0000

    add logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -29,6 +29,32 @@ export const getLogs = () => async (dispatch) => {
   }
 };
 
+// Add new log
+export const addLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
 // Set loading to true (sending to logReducer)
 export const setLoading = () => {
   return {
