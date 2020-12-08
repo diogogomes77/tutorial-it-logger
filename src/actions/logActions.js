366c8276805197ca93da6ae1ef1adf94fc35c9a1
Diogo Gomes <diogo.gomes77@gmail.com>
2020-12-08 11:10:36 +0000

    delete logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -55,6 +55,27 @@ export const addLog = (log) => async (dispatch) => {
   }
 };
 
+// delete Log from server
+export const deleteLog = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`/logs/${id}`, {
+      method: 'DELETE',
+    });
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
 // Set loading to true (sending to logReducer)
 export const setLoading = () => {
   return {
